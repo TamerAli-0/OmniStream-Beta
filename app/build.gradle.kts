f871c9d3b9640b46c156812a517e9292ba5b09c1
TamerAli-0 <blackstar09770@gmail.com>
2026-02-05 19:19:15 +0300

    Bump version to 2.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.omnistream.beta"
         minSdk = 24  // Android 7.0 - covers 95%+ devices
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.3.1"
+        versionCode = 6
+        versionName = "2.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
