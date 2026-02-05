6d236b14c44cfa48cc3490460cc09adeb52a7218
TamerAli-0 <blackstar09770@gmail.com>
2026-02-05 19:34:25 +0300

    Bump version to 2.3.4 for update checker testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.omnistream.beta"
         minSdk = 24  // Android 7.0 - covers 95%+ devices
         targetSdk = 34
-        versionCode = 7
-        versionName = "2.3.3"
+        versionCode = 8
+        versionName = "2.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
